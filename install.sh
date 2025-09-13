679f0a15afecc6e6cd2271d8006260b1f118d368
Wei Song <wsong83@gmail.com>
2025-09-14 00:47:31 +0800

    finish claim1

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -x
 source env.sh
 
 # prepare the OS
-sudo apt-get -y install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev libglib2.0-dev sshfs device-tree-compiler libboost-regex-dev libboost-system-dev libboost-program-options-dev python-dev python3-dev unzip libpixman-1-dev rsync wget cpio
+sudo apt-get -y install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev libglib2.0-dev sshfs device-tree-compiler libboost-regex-dev libboost-system-dev libboost-program-options-dev python3-dev unzip libpixman-1-dev rsync wget cpio python3-numpy python3-matplotlib
 
 # artifact
 git clone --recurse-submodules --branch sp2026-seqass https://github.com/comparch-security/spike-flexicas.git $SPIKE
