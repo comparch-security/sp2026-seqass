8512bd26f0178e775cbf6b17a7d2f3c6646bebf6
Wei Song <wsong83@gmail.com>
2025-09-13 22:48:28 +0800

    update install script

--- env.sh
+++ env.sh
@@ -3,11 +3,13 @@
 export ROOT=$PWD
 export RISCV=$ROOT/infrastructure/riscv
 export SPIKE=$ROOT/artifact/spike-flexicas
+export BALL_TEST=$ROOT/artifact/ball-test
 export CM=$SPIKE/flexicas
-export SPECKLE=$ROOT/infrastructure/speckle-2017/build/overlay
-export SPIKE_SDK=$ROOT/infrastructure/spike-sdk-spec2017
 export TOOLCHAIN=$ROOT/infrastructure/riscv-gnu-toolchain
 export SPIKE_VIRTIO=$ROOT/infrastructure/spike-virtio-devices
+export SPECKLE_REPO=$ROOT/infrastructure/speckle-2017
+export SPECKLE=$SPECKLE_REPO/build/overlay
+export SPIKE_SDK=$ROOT/infrastructure/spike-sdk-spec2017
 
 export PATH=$PATH:$ROOT/bin:$RISCV/bin
 
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+set -x
+
+# get all variables
+source env.sh
+
+# prepare the OS
+sudo apt-get -y install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev libglib2.0-dev sshfs device-tree-compiler libboost-regex-dev libboost-system-dev libboost-program-options-dev python-dev python3-dev unzip libpixman-1-dev rsync wget cpio
+
+# artifact
+git clone --recurse-submodules --branch sp2026-seqass https://github.com/comparch-security/spike-flexicas.git $SPIKE
+git clone https://github.com/comparch-security/sp2026-seqass-ball-test.git $BALL_TEST
+
+# infrastructure
+git clone --branch 2023.12.12 https://github.com/riscv-collab/riscv-gnu-toolchain.git $TOOLCHAIN
+git clone https://github.com/comparch-security/spike-virtio-devices.git $SPIKE_VIRTIO
+#git clone https://github.com/comparch-security/speckle-2017.git $SPECKLE_REPO
+git clone https://github.com/comparch-security/spike-sdk-spec2017 $SPIKE_SDK
+
+# compile spike
+install-spike
+
+# compile riscv tool chain
+install-riscv-toolchain
+
+#install spike sdk
+install-spike-sdk
+
