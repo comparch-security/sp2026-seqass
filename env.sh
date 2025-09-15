2f2715633b3ba089d3b37c8d8ff068ead0e77a51
Wei Song <wsong83@gmail.com>
2025-09-15 09:31:58 +0800

    update script

--- env.sh
+++ env.sh
@@ -8,10 +8,9 @@ export CM=$SPIKE/flexicas
 export TOOLCHAIN=$ROOT/infrastructure/riscv-gnu-toolchain
 export SPIKE_VIRTIO=$ROOT/infrastructure/spike-virtio-devices
 export SPECKLE_REPO=$ROOT/infrastructure/speckle-2017
-export SPECKLE=$SPECKLE_REPO/build/overlay
+export SPECKLE=$RISCV/spec2017/overlay
 export SPIKE_SDK=$ROOT/infrastructure/spike-sdk-spec2017
 
 export PATH=$PATH:$ROOT/bin:$RISCV/bin
 
 alias xterm-spec='xterm -e /usr/bin/bash -c "spike-spec"'
-alias xterm-mcore='xterm -e /usr/bin/bash -c "spike-mcore"'
